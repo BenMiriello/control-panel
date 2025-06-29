b1a8b51011a5003ac2d87c9aa55d6966995a6607
BenMiriello <benmiriello@gmail.com>
2025-06-29 15:02:25 -0700

    Register rest of commands

--- control_panel/cli/__init__.py
+++ control_panel/cli/__init__.py
@@ -2,21 +2,40 @@
 
 import click
 
-# Import command modules
-from . import service_commands
+# Import all command modules
+from . import service_commands, management_commands, config_commands, system_commands
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]})
 def cli():
     """Control Panel - Manage your services and ports"""
     pass
 
-# Add service lifecycle commands
+# Register service lifecycle commands
 cli.add_command(service_commands.start)
 cli.add_command(service_commands.stop)
 cli.add_command(service_commands.restart)
 cli.add_command(service_commands.enable)
 cli.add_command(service_commands.disable)
 cli.add_command(service_commands.auto)
+
+# Register service management commands
+cli.add_command(management_commands.register)
+cli.add_command(management_commands.unregister)
+cli.add_command(management_commands.list)
+cli.add_command(management_commands.logs)
+cli.add_command(management_commands.edit)
+
+# Register configuration commands
+cli.add_command(config_commands.add_range)
+cli.add_command(config_commands.backup)
+cli.add_command(config_commands.restore)
+cli.add_command(config_commands.import_config)
+
+# Register system commands
+cli.add_command(system_commands.web)
+cli.add_command(system_commands.kill_port)
+cli.add_command(system_commands.completion)
+cli.add_command(system_commands.uninstall)
 
 # Export the CLI function as main for entry_point in setup.py
 main = cli
